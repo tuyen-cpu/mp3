4abab5f8ccf61b044556075f107f66558e8ad5de
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-01-05 00:57:37 +0700

    change link mp3

--- assets/controls.js
+++ assets/controls.js
@@ -31,37 +31,37 @@ const app = {
         {
             name: "Hết thương cạn nhớ",
             singer: "Đức Phúc",
-            path: "https://vnso-zn-23-tf-mp3-s1-zmp3.zadn.vn/729125b300f4e9aab0e5/3341552255942811238?authen=exp=1641400239~acl=/729125b300f4e9aab0e5/*~hmac=7eee98d3f7ba99c8fe7ae8d7c9d00414&fs=MTY0MTIyNzQzOTmUsICxM3x3ZWJWNnwwfDExMy4xNjYdUngOTMdUngNTg",
+            path: "./assets/audio/hetthuongcannho.mp3",
             image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/cover/9/7/f/a/97fa122b55eefd723d7b97f887ee8786.jpg"
         },
         {
             name: "Đã từng",
             singer: "Bùi Anh Tuấn x Dương Hoàng Yến",
-            path: "https://vnso-zn-24-tf-mp3-s1-zmp3.zadn.vn/0b302e232667cf399676/2397390462222726590?authen=exp=1641400351~acl=/0b302e232667cf399676/*~hmac=534cf909adfe767fe29514762f25b1d0&fs=MTY0MTIyNzU1MTY0N3x3ZWJWNnwwfDExMy4xNjYdUngMjMyLjE4MQ",
+            path: "./assets/audio/datung.mp3",
             image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/covers/8/b/8b6556268f7084e30322337b91e5c2cb_1421640013.jpg"
         },
         {
             name: "24H",
             singer: "LyLy",
-            path: "https://vnso-zn-10-tf-mp3-s1-zmp3.zadn.vn/26125d0295467c182557/4987910558067477639?authen=exp=1641399934~acl=/26125d0295467c182557/*~hmac=8a2cf55d9e1c6abd599b2423a062f112&fs=MTY0MTIyNzEzNDk3Nnx3ZWJWNnwxMDAzNDA2MzA1fDI3LjmUsIC4LjI2LjE0Mg",
+            path: "./assets/audio/24h.mp3",
             image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/cover/c/5/4/d/c54df0e46c902b5b43602c57c2b77df4.jpg"
         },
         {
             name: "Không sao mà, Em đây rồi",
             singer: "Suni Hạ Linh x Lou Hoàng",
-            path: "https://vnso-zn-10-tf-mp3-s1-zmp3.zadn.vn/366fa8ff83b86ae633a9/8235770814660146380?authen=exp=1641400413~acl=/366fa8ff83b86ae633a9/*~hmac=1cb809e8d2e769f3549bafd3e092b623&fs=MTY0MTIyNzYxMzE1NXx3ZWJWNnwxMDQzMDkwMjE0fDExNi45OS4xOTgdUngMTM",
+            path: "./assets/audio/khongsaomaemdayma.mp3",
             image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/cover/4/8/f/f/48ffdb72c21e5865fa0f2f1f269f0e16.jpg"
         },
         {
             name: "Xe đạp",
             singer: "Thùy Chi x M4U",
-            path: "https://vnso-zn-10-tf-mp3-s1-zmp3.zadn.vn/607dd564df20367e6f31/7961013187244538410?authen=exp=1641400594~acl=/607dd564df20367e6f31/*~hmac=2cd1660665948291d91fa241386f18e1&fs=MTY0MTIyNzmUsIC5NDU1NHx3ZWJWNnwwfDExMy4xNjYdUngMjMyLjE4MQ",
+            path: "./assets/audio/xedap.mp3",
             image: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/avatars/2/0/2083eee4295ae84c7ab510340ff11908_1457321630.jpg"
         },
         {
             name: "Daydreams",
             singer: "Soobin Hoàng Sơn",
-            path: "https://vnso-zn-23-tf-mp3-s1-zmp3.zadn.vn/3a687f38ea7c03225a6d/7092920429194289341?authen=exp=1641400287~acl=/3a687f38ea7c03225a6d/*~hmac=b6eefcc1ad5d7e4265a79303bd41b06a&fs=MTY0MTIyNzQ4NzA0MHx3ZWJWNnwwfDExOC42OS4zNS4xMjmUsI",
+            path: "./assets/audio/daydreams.mp3",
             image: "https://znews-photo.zadn.vn/w660/Uploaded/qfssu/2021_07_16/avasoo.jpg"
         }
     ],
